935784d1a1101aa38a8ea08987248db7fd855141
so3659 <so3659@naver.com>
2024-09-26 22:42:30 +0900

    Make SpringBoot Project

---
+++ src/main/java/umc/study/springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package umc.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
